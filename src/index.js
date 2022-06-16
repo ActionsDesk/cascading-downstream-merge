bab783cbf663dc8f3a7866b77b0ba186c0c677f3
Austin Bakos <Austin.Bakos@libertymutual.com>
2022-06-16 17:18:02 -0400

    removing top level try except so we know if it is completing all the way succefully

--- src/index.js
+++ src/index.js
@@ -7,65 +7,61 @@ const github = require('@actions/github')
  * @description Entrypoint
  */
 async function exec () {
-  try {
-    const prefixes = core.getInput('prefixes')
-    const refBranch = core.getInput('refBranch')
-    const githubToken = core.getInput('GITHUB_TOKEN')
-    const mergeToken = core.getInput('MERGE_TOKEN')
+  const prefixes = core.getInput('prefixes')
+  const refBranch = core.getInput('refBranch')
+  const githubToken = core.getInput('GITHUB_TOKEN')
+  const mergeToken = core.getInput('MERGE_TOKEN')
 
-    const octokit = github.getOctokit(githubToken)
+  const octokit = github.getOctokit(githubToken)
 
-    let mergeOctokit
-    if (mergeToken) {
-      console.log('Got a merge token. Creating seperate octokit object.')
-      mergeOctokit = github.getOctokit(mergeToken)
-    } else {
-      mergeOctokit = octokit
-    }
+  let mergeOctokit
+  if (mergeToken) {
+    console.log('Got a merge token. Creating seperate octokit object.')
+    mergeOctokit = github.getOctokit(mergeToken)
+  } else {
+    mergeOctokit = octokit
+  }
 
-    const context = github.context
-    const owner = github.context.repo.owner
-    const repo = github.context.repo.repo
-    const pullNumber = context.payload.pull_request.number
-    const headBranch = context.payload.pull_request.head.ref
-    const baseBranch = context.payload.pull_request.base.ref
-    const actor = context.actor
+  const context = github.context
+  const owner = github.context.repo.owner
+  const repo = github.context.repo.repo
+  const pullNumber = context.payload.pull_request.number
+  const headBranch = context.payload.pull_request.head.ref
+  const baseBranch = context.payload.pull_request.base.ref
+  const actor = context.actor
 
-    const prefixArray = prefixes.split(',')
+  const prefixArray = prefixes.split(',')
 
-    console.log('owner: ' + owner)
-    console.log('repo: ' + repo)
-    console.log('actor: ' + actor)
-    console.log('prefixes: ' + prefixes)
-    console.log('prefixArray: ', prefixArray)
-    console.log('refBranch: ' + refBranch)
-    console.log('pullNumber: ' + pullNumber)
-    console.log('headBranch: ' + headBranch)
-    console.log('baseBranch: ' + baseBranch)
+  console.log('owner: ' + owner)
+  console.log('repo: ' + repo)
+  console.log('actor: ' + actor)
+  console.log('prefixes: ' + prefixes)
+  console.log('prefixArray: ', prefixArray)
+  console.log('refBranch: ' + refBranch)
+  console.log('pullNumber: ' + pullNumber)
+  console.log('headBranch: ' + headBranch)
+  console.log('baseBranch: ' + baseBranch)
 
-    console.log(context)
+  console.log(context)
 
-    const repository = {
-      owner,
-      repo
-    }
-    if (context.payload.pull_request.merged) {
-      cascadingBranchMerge(
-        prefixArray,
-        refBranch,
-        headBranch,
-        baseBranch,
-        repository,
-        octokit,
-        mergeOctokit,
-        pullNumber,
-        actor
-      )
-    } else {
-      console.log('PR was not merged. Skipping cascade.')
-    }
-  } catch (e) {
-    console.log(e)
+  const repository = {
+    owner,
+    repo
+  }
+  if (context.payload.pull_request.merged) {
+    cascadingBranchMerge(
+      prefixArray,
+      refBranch,
+      headBranch,
+      baseBranch,
+      repository,
+      octokit,
+      mergeOctokit,
+      pullNumber,
+      actor
+    )
+  } else {
+    console.log('PR was not merged. Skipping cascade.')
   }
 }
 
